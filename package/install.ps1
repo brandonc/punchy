2f71cff1a4d9d83eaf89be1e6b6bfbdd3dbfdd97
brandonc <brandon.croft@gmail.com>
2011-04-29 10:09:23 -0600

    nuget package, redirect debug build output

---
+++ package/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+$project.Object.References.Add("Punchy"); 
+$project.Object.References.Add("Punchy.Plugin.DotLessCss"); 
+$project.Object.References.Add("Punchy.Plugin.GoogleClosure"); 
+$project.Object.References.Add("Punchy.Plugin.MicrosoftAjaxMinifier"); 
+$project.Object.References.Add("Punchy.Plugin.YahooYuiCompressor"); 
